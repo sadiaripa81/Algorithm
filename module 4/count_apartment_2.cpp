c1dbe6f7cabc5c44bd349b53895d4c05569e4f9c
sadia <sadiaripa81@gmail.com>
2025-08-23 23:34:39 +0600

    count apartment 2

--- module 4/count_apartment_2.cpp
+++ module 4/count_apartment_2.cpp
@@ -5,8 +5,6 @@ char grid[1005][1005];
 bool vis[1005][1005];
 vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
 int n, m;
-int si, sj;
-bool found_B = false;
 
 bool valid(int i, int j)
 {
@@ -19,58 +17,56 @@ bool valid(int i, int j)
         return true;
     }
 }
-void dfs(int si, int sj)
+int dfs(int si, int sj)
 {
     vis[si][sj] = true;
-
-    if (grid[si][sj] == 'B')
-    {
-        found_B = true;
-        return;
-    }
-
+    int cnt = 1;
     for (int i = 0; i < 4; i++)
     {
         int ci = si + d[i].first;
         int cj = sj + d[i].second;
-        if (valid(ci, cj) == true && !vis[ci][cj] && (grid[ci][cj] == '.' || grid[ci][cj] == 'B'))
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
         {
-            dfs(ci, cj);
-            if (found_B == true)
-            {
-                return;
-            }
+
+            cnt += dfs(ci, cj);
         }
     }
+    return cnt;
 }
 int main()
 {
     cin >> n >> m;
-
     for (int i = 0; i < n; i++)
         for (int j = 0; j < m; j++)
             cin >> grid[i][j];
 
+    memset(vis, false, sizeof(vis));
+    vector<int> apartments;
+
     for (int i = 0; i < n; i++)
+    {
         for (int j = 0; j < m; j++)
         {
-            if (grid[i][j] == 'A')
+            if (vis[i][j] == false && grid[i][j] == '.')
             {
-                si = i;
-                sj = j;
+                int room = dfs(i, j);
+                apartments.push_back(room);
             }
         }
-
-    if (si == -1 || sj == -1)
-    {
-        cout << "NO" << endl;
     }
-    memset(vis, false, sizeof(vis));
-    dfs(si, sj);
 
-    if (found_B == true)
-        cout << "YES" << endl;
+    sort(apartments.begin(), apartments.end());
+
+    if (apartments.empty())
+    {
+        cout << 0 << endl;
+    }
     else
-        cout << "NO" << endl;
+    {
+        for (int size : apartments)
+        {
+            cout << size << " ";
+        }
+    }
     return 0;
 }
