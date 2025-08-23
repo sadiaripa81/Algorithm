acc0d061a500a636299d52a742386136ce2dd35a
sadia <sadiaripa81@gmail.com>
2025-08-23 23:13:24 +0600

    counting apartment

---
+++ module 4/tempCodeRunnerFile.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grid[105][105];
+bool vis[105][105];
+vector<pair<int, int>> d = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+void dfs(int si, int sj)
+{
+    vis[si][sj] = true;
+
+    for (int i = 0; i < 4; i++)
+    {
+        int ci = si + d[i].first;
+        int cj = sj + d[i].second;
+        if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] == '.')
+        {
+            dfs(ci, cj);
+        }
+    }
+}
+int main()
+{
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> grid[i][j];
+
+    memset(vis, false, sizeof(vis));
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (vis[i][j] == false && grid[i][j] == '.')
+            {
+                count++;
+                dfs(i, j);
+            }
+        }
+    }
+    cout << count;
+    return 0;
+}
