39272b532ee3c256fe4d6191349f02590c11ec88
sadia <sadiaripa81@gmail.com>
2025-08-31 22:35:00 +0600

    maze puzzle

---
+++ module 8 mid/maze.cpp
@@ -0,0 +1,101 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+char grid[1005][1005];
+bool vis[1005][1005];
+int level[1005][1005];
+pair<int, int> parent[1005][1005];
+vector<pair<int, int>> d = {{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+int n, m;
+
+bool valid(int i, int j)
+{
+    if (i < 0 || i >= n || j < 0 || j >= m)
+        return false;
+    return true;
+}
+void bfs(int si, int sj)
+{
+    queue<pair<int, int>> q;
+    q.push({si, sj});
+    vis[si][sj] = true;
+    level[si][sj] = 0;
+    parent[si][sj] = {-1, -1};
+
+    while (!q.empty())
+    {
+        pair<int, int> par = q.front();
+        q.pop();
+        int pi = par.first;
+        int pj = par.second;
+
+        for (int i = 0; i < 4; i++)
+        {
+            int ci = pi + d[i].first;
+            int cj = pj + d[i].second;
+            if (valid(ci, cj) && !vis[ci][cj] && grid[ci][cj] != '#')
+            {
+                q.push({ci, cj});
+                vis[ci][cj] = true;
+                level[ci][cj] = level[pi][pj] + 1;
+                parent[ci][cj] = {pi, pj};
+            }
+        }
+    }
+}
+int main()
+{
+    cin >> n >> m;
+    int si , sj, di , dj ;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cin >> grid[i][j];
+            if (grid[i][j] == 'R')
+            {
+                si = i;
+                sj = j;
+            }
+            else if (grid[i][j] == 'D')
+            {
+                di = i;
+                dj = j;
+            }
+        }
+    }
+    memset(vis, false, sizeof(vis));
+    memset(level, -1, sizeof(level));
+    memset(parent, -1, sizeof(parent));
+
+    bfs(si, sj);
+
+    if (vis[di][dj] == true)
+    {
+        int node_i = di, node_j = dj;
+        while (1)
+        {
+            pair<int, int> par = parent[node_i][node_j];
+            node_i = par.first;
+            node_j = par.second;
+
+            if (grid[node_i][node_j] == 'R')
+            {
+                break;
+            }
+            else
+            {
+                grid[node_i][node_j] = 'X';
+            }
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            cout << grid[i][j];
+        }
+        cout << endl;
+    }
+    return 0;
+}
